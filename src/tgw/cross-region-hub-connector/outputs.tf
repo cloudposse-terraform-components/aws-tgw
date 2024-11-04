0c085467b04c971bf78ef54a0c6534899a48f2e8
Igor Rodionov <goruha@gmail.com>
2024-11-04 01:03:23 +0100

    Initial commit

--- src/tgw/cross-region-hub-connector/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_ec2_transit_gateway_peering_attachment_id" {
-  value       = join("", aws_ec2_transit_gateway_peering_attachment.this[*].id)
-  description = "Transit Gateway Peering Attachment ID"
-}
